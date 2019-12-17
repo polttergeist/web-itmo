4dda3c1b29b96173c89c568fc477b78b145c51d0
polttergeist <ivan010201@gmail.com>
2019-12-17 11:21:28 +0300

    Add files via upload

---
+++ new 1.js
@@ -0,0 +1,7 @@
+let ITMO = confirm('Are you a gay?')
+if ITMO{
+	alert("You're welcome!")
+}
+else {
+	alert('Get the fuck off of this page!')
+}
---
+++ new_1.js
@@ -0,0 +1,18 @@
+/*while (confirm('Are you a gay?')){alert("You're welcome!");};
+alert('Fin.')*/
+/*function ku() {
+	alert('Дороу.');
+}
+
+let dorou = ku;
+ku();
+dorou();*/
+console.profile();
+console.time("Let's go ahead!");
+ function dis() {
+	 let fo = document.getElementsByTagName('form')[0];
+	 fo.disabled = true;
+ }
+ console.timeEnd("Let's go ahead!")
+ console.profileEnd();
+ 
